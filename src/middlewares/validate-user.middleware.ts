218f23e8f2804f9f79eefd9d93ca60df3f3d6105
mahmoudnabil133 <hodanabil144@gmail.com>
2024-10-02 09:10:09 +0300

    Add new middleware to validate user account

---
+++ src/middlewares/validate-user.middleware.ts
@@ -0,0 +1,12 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { Request, Response, NextFunction } from "express";
+
+@Injectable()
+export class ValidateUserMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: NextFunction) {
+        console.log('validate user middleware');
+        const { authorization } = req.headers;
+        if (authorization === '123') next();
+        else res.status(403).send({ msg : 'invalid authorization token'})
+    }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,27 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { ValidateUserMiddleware } from 'src/middlewares/validate-user.middleware';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    // consumer
+    // .apply(ValidateUserMiddleware)
+    // .forRoutes({
+    //   path: 'users/:id',
+    //   method: RequestMethod.GET
+    // });
+    
+    consumer
+    .apply(ValidateUserMiddleware)
+    .exclude({
+      path: 'users',
+      method: RequestMethod.POST
+    })
+    .forRoutes(UsersController);
+  }
+}
